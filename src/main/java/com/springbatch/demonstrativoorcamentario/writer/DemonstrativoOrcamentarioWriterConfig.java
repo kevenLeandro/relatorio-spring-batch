ea4e04d2f93c8c0d396fe6676345b0bd0d57c4d3
keven.santos <keven.santos@betha.com.br>
2023-09-21 11:26:17 -0300

    add file

---
+++ src/main/java/com/springbatch/demonstrativoorcamentario/writer/DemonstrativoOrcamentarioWriterConfig.java
@@ -0,0 +1,78 @@
+package com.springbatch.demonstrativoorcamentario.writer;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileFooterCallback;
+import org.springframework.batch.item.file.FlatFileHeaderCallback;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.batch.item.file.transform.LineAggregator;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.springbatch.demonstrativoorcamentario.dominio.GrupoLancamento;
+import com.springbatch.demonstrativoorcamentario.dominio.Lancamento;
+import org.springframework.core.io.Resource;
+
+@Configuration
+public class DemonstrativoOrcamentarioWriterConfig {
+    @StepScope
+  @Bean
+	public FlatFileItemWriter<GrupoLancamento> demonstrativoOrcamentarioWriter(
+          @Value("#{jobParameters['demostrativoOrcamentario']}") Resource resource,
+          DemostrativoOrçamentarioRodape rodapeCallback
+
+  ){
+
+      return  new FlatFileItemWriterBuilder<GrupoLancamento>()
+              .name("demonstrativoOrcamentarioWriter")
+              .resource(resource)
+              .lineAggregator(lineAggregator())
+              .headerCallback(cabecalhoCallback())
+              .footerCallback(rodapeCallback)
+              .build();
+  }
+
+    private FlatFileHeaderCallback cabecalhoCallback() {
+        return new FlatFileHeaderCallback() {
+            @Override
+            public void writeHeader(Writer writer) throws IOException {
+                writer.append(String.format("SISTEMA INTEGRADO: XPTO \t\t\t\t DATA: %s\n", new SimpleDateFormat("dd/MM/yyyy").format(new Date())));
+                writer.append(String.format("MÓDULO: ORÇAMENTO \t\t\t\t\t HORA: %s\n", new SimpleDateFormat("HH:MM").format(new Date())));
+                writer.append(String.format("\t\t\tDEMONSTRATIVO ORCAMENTARIO\n"));
+                writer.append(String.format("----------------------------------------------------------------------------\n"));
+                writer.append(String.format("CODIGO NOME VALOR\n"));
+                writer.append(String.format("\t Data Descricao Valor\n"));
+                writer.append(String.format("----------------------------------------------------------------------------\n"));
+            }
+        } ;
+        }
+
+    private LineAggregator<GrupoLancamento> lineAggregator() {
+       return new LineAggregator<GrupoLancamento>() {
+           @Override
+           public String aggregate(GrupoLancamento grupo) {
+               String formatoGruppoLancamento = String.format("[%d] %s - %s\n", grupo.getCodigoNaturezaDespesa(),
+                       grupo.getDescricaoNaturezaDespesa(),
+                       NumberFormat.getCurrencyInstance().format(grupo.getTotal()));
+
+               StringBuilder stringBuilder = new StringBuilder();
+               for (Lancamento lancamento : grupo.getLancamentos()) {
+                   stringBuilder.append(String.format("\t [%s] %s - %s\n", new SimpleDateFormat("dd/MM/yyyy").format(lancamento.getData()), lancamento.getDescricao(),
+                           NumberFormat.getCurrencyInstance().format(lancamento.getValor())));
+               }
+                String formatLancamento = stringBuilder.toString();
+               return formatoGruppoLancamento + formatLancamento;
+           }
+       };
+    }
+
+}
+
---
+++ src/main/resources/peter.sql
@@ -0,0 +1,9 @@
+CREATE TABLE xpto.lancamento (
+	descricao varchar(100) NOT NULL,
+	`Date` DATE NULL,
+	valor DOUBLE NOT NULL,
+	id INTEGER NULL
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_0900_ai_ci;
